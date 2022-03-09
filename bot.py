d0f29965d4a0a35b58d7b327aaecd43e89ab11d7
fetageta6 <95299865+fetageta6@users.noreply.github.com>
2022-03-09 12:59:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from bot import LOG, app, chats_data, from_chats
 
 LOG.info("Welcome, this is the telegram-message-forwarder-bot. main routine...")
 
-@app.on_message(filters.chat(from_chats) & filters.incoming & filters.photo)
+@app.on_message(filters.chat(from_chats) & filters.photo)
 def work(client, message):
     caption = None
     msg = None
