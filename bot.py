72d4d0d226ffcc6dc15e6bce3410b3f365633158
fetageta6 <95299865+fetageta6@users.noreply.github.com>
2022-03-09 12:44:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
-import os
 from pyrogram import filters
-from bot import LOG, app, advance_config, chats_data, from_chats
+from bot import LOG, app, chats_data, from_chats
 
 LOG.info("Welcome, this is the telegram-message-forwarder-bot. main routine...")
 
@@ -8,14 +7,13 @@ LOG.info("Welcome, this is the telegram-message-forwarder-bot. main routine...")
 def work(client, message):
     caption = None
     msg = None
-    if advance_config:
-        try:
-            for chat in chats_data[message.chat.id]:
-                if caption:
-                    message.copy(chat, caption=caption)
-                else:
-                    message.copy(chat)
-        except Exception as e:
-            LOG.error(e)
+    try:
+        for chat in chats_data[message.chat.id]:
+            if caption:
+                message.copy(chat, caption=caption)
+            else:
+                message.copy(chat)
+    except Exception as e:
+        LOG.error(e)
 
 app.run()
