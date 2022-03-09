b085f25b3dcb21260f58ed64403198f6379c6420
fetageta6 <95299865+fetageta6@users.noreply.github.com>
2022-03-09 13:15:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,14 +5,9 @@ LOG.info("Welcome, this is the telegram-message-forwarder-bot. main routine...")
 
 @app.on_message(filters.chat(from_chats) & filters.photo)
 def work(client, message):
-    caption = None
-    msg = None
     try:
         for chat in chats_data[message.chat.id]:
-            if caption:
-                message.copy(chat, caption=caption)
-            else:
-                message.copy(chat)
+            message.copy(chat)
     except Exception as e:
         LOG.error(e)
 
