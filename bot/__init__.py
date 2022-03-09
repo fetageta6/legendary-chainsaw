eddcc18ada9209a8bd42cf23fa6df11add1b706d
fetageta6 <95299865+fetageta6@users.noreply.github.com>
2022-03-09 11:59:25 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,21 +1,45 @@
 import os
-from pyrogram import filters
-from bot import LOG, app, advance_config, chats_data, from_chats
+import sys
+import logging
+from os import environ
+from dotenv import load_dotenv
+from pyrogram import Client
 
-LOG.info("Welcome, this is the telegram-message-forwarder-bot. main routine...")
+log_level = environ.get("LOG_LEVEL", "INFO").upper()
+logging.basicConfig(format='[%(asctime)s - %(pathname)s - %(levelname)s] %(message)s',
+                    handlers=[logging.FileHandler('log.txt'), logging.StreamHandler()],
+                    level=log_level)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+LOG = logging.getLogger(__name__)
 
-@app.on_message(filters.chat(from_chats) & filters.incoming & filters.photo)
-def work(client, message):
-    caption = None
-    msg = None
+chats_data = {}
+
+LOG.info("Welcome, this is the telegram-message-forwarder-bot. initializing...")
+
+api_id = int(environ["API_ID"])
+api_hash = environ["API_HASH"]
+tg_session = environ.get("TELEGRAM_SESSION", None)
+advance_config = environ.get("ADVANCE_CONFIG", None)
+from_chats = []
+
+if tg_session:
+  LOG.info("Session Mode - {tg_session}")
+  app = Client(tg_session, api_id, api_hash)
+  
+with app:
     if advance_config:
-        try:
-            for chat in chats_data[message.chat.id]:
-                if caption:
-                    message.copy(chat, caption=caption)
-                else:
-                    message.copy(chat)
-        except Exception as e:
-            LOG.error(e)
-
-app.run()
+    for chats in advance_config.split(";"):
+      chat = chats.strip().split()
+      f = int(chat[0])
+      del chat[0]
+      chat = [int(i) for i in chat]
+      if f in chats_data:
+        c = chats_data[f]
+        c.extend(chat)
+        chats_data[f] = c
+      else:
+        chats_data[f] = chat
+      if not f in from_chats:
+        from_chats.append(f)
+    LOG.info(f"From Chats: {from_chats}")
+    LOG.info(f"Advanced Config: {chats_data}")
